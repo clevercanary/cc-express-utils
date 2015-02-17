ddb3743e95d935df304673fdf29599d9d4a69845
MillenniumFalconMechanic <mim@excira.com>
2015-02-17 13:43:49 -0500

    Added README.

---
+++ README.md
@@ -0,0 +1,4 @@
+CC Express Utils
+=================
+
+Core Express-related utility functionality.
--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-'use strict';
+/**
+ * Express-related utils
+ */
 
+"use strict";
+
+/**
+ * Create response callback function, returning either 500 on error
+ * or 200 with data.
+ *
+ * Called from web controllers. Pass to service layer as a callback to
+ * be invoked once service has completed coordinating/mediating.
+ *
+ * @param formatFn Function for formatting response value. Optional.
+ */
 exports.setupResponseCallback = function (res, formatFn) {
 
     return function (error, returnValue) {
@@ -14,24 +27,5 @@ exports.setupResponseCallback = function (res, formatFn) {
         }
 
         res.json(200, returnValue);
-    };
-};
-
-//
-// Create customer error message for the specified field in
-// the format:
-// {
-//     message: "My error message"
-// }
-//
-function createFormFieldErrorMessage(fieldName, message) {
-
-    var errors = {};
-    errors[fieldName] = {
-        message: message
-    };
-
-    return {
-        errors:errors
     };
 };
