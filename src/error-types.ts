f34b112448a262dab6785e544820764fb2644744
cdbajorin <chris@clevercanary.com>
2016-08-24 12:52:08 -0400

    handle error extension for es5 compilation target

--- src/error-types.ts
+++ src/error-types.ts
@@ -4,9 +4,23 @@
 import { Dictionary } from "./typings";
 
 /**
+ * Error used for extension. ES5 can't inherit Error directly without some hacking.
+ * (this.constructor as any) is to handle typescript es5 lib errors.
+ */
+export class ExtendableError extends Error {
+
+    constructor(message: string) {
+        super(message);
+        this.name = (this.constructor as any).name;
+        this.message = message;
+        Error.captureStackTrace(this, (this.constructor as any));
+    }
+}
+
+/**
  * Express Response Error With HTTP Status Code
  */
-export class ResponseError extends Error {
+export class ResponseError extends ExtendableError {
 
     statusCode: number;
 
@@ -44,7 +58,7 @@ export interface ValidatorError {
     msg: string;
     value: any;
 }
-export class ValidationError extends Error {
+export class ValidationError extends ExtendableError {
 
     errors: ValidatorError[];
     statusCode: number = 422;
